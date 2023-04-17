5271307236a077172e3d858b4c316369e2da39b6
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-04-17 19:25:05 +0700

    Upload Data

---
+++ bundle/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let namasaya = "Zefri Fahlevi";
+let ID = 1234;
+let isDead = false;
+let roleplay = "Drian Matteus";
+console.log({ namasaya });
+console.log({ ID });
+console.log({ isDead });
+console.log({ roleplay });
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+let namasaya: string = "Zefri Fahlevi";
+let ID: number = 1234;
+let isDead: boolean = false;
+
+let roleplay = "Drian Matteus";
+
+console.log({ namasaya });
+console.log({ ID });
+console.log({ isDead });
+console.log({ roleplay });
