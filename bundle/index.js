cc38d46e90205835104bba5abdd4c2d51c01da99
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-04-17 20:22:19 +0700

    Updating

--- bundle/index.js
+++ bundle/index.js
@@ -4,10 +4,19 @@ let ID = 1234;
 let isDead = false;
 let roleplay = "Drian Matteus";
 //contoh variabel bisa tipe data
+// let diriku: number | string | boolean;
+// diriku = 14;
+// diriku = "Patrzalek";
+// diriku = true;
+// array biasa
 let diriku;
-diriku = "Tanggal Lahir";
+diriku = ["James", "Ramu"];
+// tuple array
+let oranglain;
+oranglain = ["Patrzalek", 2206, true];
 console.log({ namasaya });
 console.log({ ID });
 console.log({ isDead });
 console.log({ roleplay });
 console.log({ diriku });
+console.log({ oranglain });
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,22 @@ let isDead: boolean = false;
 let roleplay = "Drian Matteus";
 
 //contoh variabel bisa tipe data
-let diriku: number | string;
+// let diriku: number | string | boolean;
+// diriku = 14;
+// diriku = "Patrzalek";
+// diriku = true;
 
-diriku = "Tanggal Lahir";
+// array biasa
+let diriku: string[];
+diriku = ["James", "Ramu"];
+
+// tuple array
+let oranglain: [string, number, boolean];
+oranglain = ["Patrzalek", 2206, true]
 
 console.log({ namasaya });
 console.log({ ID });
 console.log({ isDead });
 console.log({ roleplay });
 console.log({ diriku });
+console.log({ oranglain });
