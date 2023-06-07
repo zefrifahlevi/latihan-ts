6a6966f6cac830760a5768e9ca9117dbc028cabb
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-06-07 17:33:22 +0700

    Update Script

--- src/index.tsx
+++ src/index.tsx
@@ -102,7 +102,7 @@ let diriku: string = "Be Yourself"
 let today: boolean = true  // True | False
 let yesterday: boolean = true // True
 
-if (yesterday = today ) {
+if (yesterday = today) {
     console.log("Zefri's Way..");
     console.log(`Not Different You Are In The Right Path (${diriku})`);
 } 
@@ -110,3 +110,4 @@ else {
     console.log("Zefri's Way..");
     console.log("Today Is Different From Yesterday");
 }
+
