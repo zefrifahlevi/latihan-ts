e1135372fd7af83d9ce6414b4233cc277c1d1df2
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-06-28 11:32:58 +0700

    Update...

--- zefri.ts
+++ zefri.ts
@@ -10,22 +10,44 @@ interface Intel extends IProcessor {
 
 }
 
-function spekLaptop (data: IProcessor): void {
+function spekLaptopzefri (data: IProcessor): void {
     console.log(`
-    Speksifikasi dari Laptop Zefri
-    Nama Perusahaan ${data.brandname}
-    Tipe Laptop ${data.baseModel}
-    Jenis Laptop ${data.modelName}
-    Benchmark ${data.clockSpeed}
+    Spesifikasi dari Laptop Zefri
+    Nama Perusahaan: ${data.brandname}
+    Tipe Laptop: ${data.baseModel}
+    Jenis Laptop: ${data.modelName}
+    Benchmark = ${data.clockSpeed}
     turboApps? = ${data.turboBoost}
     `)
 };
 
-const intelI5 = {
+const airM1 = {
     brandname: "Apple MacBook",
     baseModel: "MacBook",
-    modelName: "MacBook Pro Air M1",
+    modelName: "Pro Air M1",
     clockSpeed: 200,
     turboBoost: true,
 }
-spekLaptop(intelI5);
+spekLaptopzefri(airM1);
+
+
+function spekLaptopzefa (data2: IProcessor): void {
+    console.log(`
+    Spesifikasi dari Laptop zefa
+    Nama Perusahaan: ${data2.brandname}
+    Tipe Laptop: ${data2.baseModel}
+    Jenis Laptop: ${data2.modelName}
+    Benchmark = ${data2.clockSpeed}
+    turboApps? = ${data2.turboBoost}
+    `)
+};
+
+const tufGaming = {
+    brandname: "ASUS",
+    baseModel: "ASUS TUF GAMING",
+    modelName: "FX-505DT",
+    clockSpeed: 500,
+    turboBoost: true,
+}
+
+spekLaptopzefa(tufGaming)
