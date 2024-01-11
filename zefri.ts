2a50f9bed015f6f07fb5dd587d7d9bd26e920a51
zefrifahlevi <zefrifahlevi@gmail.com>
2024-01-11 17:42:54 +0700

    Update Data

--- zefri.ts
+++ zefri.ts
@@ -6,9 +6,59 @@ interface IProcessor {
     turboBoost: boolean;
 }
 
+function spekLaptopzefri (data2: IProcessor): void {
+    console.log(`
+    Spesifikasi dari Laptop Zeffri
+    Nama Perusahaan: ${data2.brandname}
+    Tipe Laptop: ${data2.baseModel}
+    Jenis Laptop: ${data2.modelName}
+    Benchmark = ${data2.clockSpeed}
+    turboApps = ${data2.turboBoost}
+    `)
+};
+
+const tufGaming = {
+    brandname: "ASUS",
+    baseModel: "ASUS TUF GAMING",
+    modelName: "FX-505DT",
+    clockSpeed: 500,
+    turboBoost: true,
+}
+spekLaptopzefri(tufGaming)
+
+interface IMusic {
+    brandName: string;
+    modelName: string;
+    basicModel: string;
+    rateModel: number;
+}
+
+function spekPianozefri (data: IMusic): void {
+    console.log(`
+    Speksifikasi Piano: ${data.brandName}
+    Produk Piano: ${data.modelName}
+    Base Model: ${data.basicModel}
+    Rating Model: ${data.rateModel}
+    `)
+};
+
+const Piano = {
+    brandName: "Yamaha",
+    modelName: "PSR-E473",
+    basicModel: "Keyboard",
+    rateModel: 90
+}
+
+spekPianozefri(Piano)
+
+
+
+/* -------Berubah Pikiran-----------
+
 interface Intel extends IProcessor {
 
 }
+
 
 function spekLaptopzefri (data: IProcessor): void {
     console.log(`
@@ -29,24 +79,4 @@ const airM1 = {
     turboBoost: true,
 }
 spekLaptopzefri(airM1);
-
-
-function spekLaptopzefa (data2: IProcessor): void {
-    console.log(`
-    Spesifikasi dari Laptop Zefa
-    Nama Perusahaan: ${data2.brandname}
-    Tipe Laptop: ${data2.baseModel}
-    Jenis Laptop: ${data2.modelName}
-    Benchmark = ${data2.clockSpeed}
-    turboApps = ${data2.turboBoost}
-    `)
-};
-
-const tufGaming = {
-    brandname: "ASUS",
-    baseModel: "ASUS TUF GAMING",
-    modelName: "FX-505DT",
-    clockSpeed: 500,
-    turboBoost: true,
-}
-spekLaptopzefa(tufGaming)
+*/
