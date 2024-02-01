950ccf6753720ab02f5ccc51c22068e601d273ab
zefrifahlevi <zefrifahlevi@gmail.com>
2024-02-01 09:50:36 +0700

    Update Script

--- quotes.ts
+++ quotes.ts
@@ -1,6 +1,6 @@
 let zefri1: string = "Be Yourself"
-let today: boolean = true  // True | False
-let yesterday: boolean = true // True
+let today1: boolean = true  // True | False
+let yesterday1: boolean = true // True
 
 if (yesterday = today) {
     console.log("Zefri's Way..");
@@ -9,4 +9,17 @@ if (yesterday = today) {
 else {
     console.log("Zefri's Way..");
     console.log("Today Is Different From Yesterday");
+}
+
+const zefri2: string = "Keep Walking";
+let today2: boolean = true // True | False
+let yesterday2 : boolean = true // True | False
+
+if (yesterday = today) {
+    console.log("Zefri's Way..");
+    console.log(`Not Different You Are In The Right Path (${zefri2})`);
+}
+else {
+    console.log("Zefri' Way..");
+    console.log(`Today Is Different From Yesterday`);  
 }
