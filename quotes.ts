b201d53ddaba93512af60fca3d40727fa035cf82
zefrifahlevi <zefrifahlevi@gmail.com>
2024-02-05 13:42:17 +0700

    Adding Script

--- quotes.ts
+++ quotes.ts
@@ -2,7 +2,7 @@ let zefri1: string = "Be Yourself"
 let today1: boolean = true  // True | False
 let yesterday1: boolean = true // True
 
-if (yesterday = today) {
+if (yesterday1 = today1) {
     console.log("Zefri's Way..");
     console.log(`Not Different You Are In The Right Path (${zefri1})`);
 } 
@@ -22,4 +22,18 @@ if (yesterday2 = today2) {
 else {
     console.log("Zefri' Way..");
     console.log(`Today Is Different From Yesterday`);  
+}
+
+const zefri3: string = "Keep Sprit"
+let today3: boolean = true // True | False 
+let yesterday3: boolean =  true // True (only)
+
+if (yesterday3 = today3) {
+    console.log("Zefri's Way");
+    console.log(`Not Different You Are In The Right Path (${zefri3})`);
+}
+
+else {
+    console.log("Zefri's Way");
+    console.log(`Today Is Different From Yesterday`);
 }
